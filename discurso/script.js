fd270a589b8c6e4eba128aa7de35ed307dc10ce4
IanTalmacs <iantalmacs@gmail.com>
2019-10-13 16:11:20 -0300

    save button downloads a new file

--- discurso/script.js
+++ discurso/script.js
@@ -38,6 +38,18 @@ function checkEdits() {
 }
 
 
+function saveall() {
+    var htmlContent = [document.documentElement.innerHTML];
+    var bl = new Blob(htmlContent, { type: "text/html" });
+    var a = document.createElement("a");
+    a.href = URL.createObjectURL(bl);
+    a.download = "your-download-name-here.html";
+    a.hidden = true;
+    document.body.appendChild(a);
+    a.innerHTML = "something random - nobody will see this, it doesn't matter what you put here";
+    a.click();
+}
+
 
 
 var sw = {
