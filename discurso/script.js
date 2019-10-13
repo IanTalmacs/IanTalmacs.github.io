6012364440b4ad0c64ab3ca90d614e0133c91949
IanTalmacs <iantalmacs@gmail.com>
2019-10-13 16:15:37 -0300

    salva com título do discurso

--- discurso/script.js
+++ discurso/script.js
@@ -43,7 +43,7 @@ function saveall() {
     var bl = new Blob(htmlContent, { type: "text/html" });
     var a = document.createElement("a");
     a.href = URL.createObjectURL(bl);
-    a.download = "your-download-name-here.html";
+    a.download = document.getElementById('tema').innerHTML;
     a.hidden = true;
     document.body.appendChild(a);
     a.innerHTML = "something random - nobody will see this, it doesn't matter what you put here";
